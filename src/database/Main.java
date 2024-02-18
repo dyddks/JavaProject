f68efeb6458086ba35da10ab2fe74098391dbd5a
DESKTOP-KSUP4OT\dyddk <dyddks0115@naver.com>
2024-02-18 21:50:26 +0900

    feat: jdbc-mysql 연습

---
+++ src/database/Main.java
@@ -0,0 +1,8 @@
+package src.database;
+
+public class Main {
+    public static void main(String[] args) {
+        DBConnection connection = new DBConnection();
+        System.out.println("관리자 여부: " + connection.isAdmin("admin", "admin"));
+    }
+}
